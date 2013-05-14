54f5a0f3fb6ff2ade4051838ce361a0c0f071f14
Jake Coxon <jake.coxon@gmail.com>
2013-05-14 18:44:50 +0200

    Update microblaze_0.c

--- microblaze_0.c
+++ microblaze_0.c
@@ -6,12 +6,31 @@
 
 DECLARE_INTERRUPT_HANDLER(int_handler);
 
-void process_packet(void);
+void process_packet(volatile int *buffer);
 void send_test(void);
 
-int flash = 1;
-int channel_id = 5;
+#define PS(x) uart_send_string(UART, x);
+#define PC(x) uart_send_char(UART, x);
+
+
 int to_initialise = 0;
+int channels[] = {0,0,0,0};
+int num_channels = 0;
+volatile int *buffer;
+
+
+
+void set_channel_switches() { 
+  int switches = (int) get_switches();
+
+ 
+  if ((switches & 0b1000) == 0b1000)  // Task 1 switches
+    channels[0] = (switches & 0b111) + 5;
+  else                                // Task 2 switches
+    channels[0] = (switches & 0b11) + 1;
+  
+  num_channels = 1;
+}
 
 
 void int_handler() {
@@ -22,60 +41,68 @@ void int_handler() {
       break;
     case INTC_SWITCHES:
       switches_clear_interrupt();
+
       to_initialise = 1;
-      channel_id = ((int) get_switches()) + 1;
-      uart_print_hex_16(UART, channel_id);
-      //send_test();
-      set_leds(flash);
-      flash = !flash;
+      set_channel_switches();
+
       break;
       
     case INTC_MAC:
-      process_packet();
+      buffer = mac_packet_ready(); 
+      // Process all data packets
+      while (buffer != (void *) 0) {
+        process_packet(buffer);
+        buffer = mac_packet_ready();
+      }
       break;
   }
   intc_acknowledge_interrupt(vec);
 }
 
 
-void process_packet() {
+
+void process_packet(volatile int *buffer) {
   int temp, i;
-  volatile int *buffer, *dataptr;
+  volatile int *dataptr;
  
   buffer             = mac_packet_ready();
-  temp               = *(buffer+3);
+  temp               = *(buffer + 3);
+
+  int mix_number = -1;
+  for (i = 0; i < num_channels && mix_number == -1; i ++) {
+    if (temp == (0x55AA0000 | channels[i])) mix_number = i;
+  }
   
-  if (temp == (0x55AA0000 | channel_id)) {
+  if (mix_number > -1) {
   
-    // 11111111     11111111      11111111  11111111
-    // sample rate  sample width  reserved  reserved
+    // sample rate  sample width  mix  mix id  num count  unused
+    // 11111111     11111111      1    11      111        1111111111
     temp             = *(buffer + 4);
+
+    temp            |= (mix_number & 0b11)    << 13;
+    temp            |= (num_channels & 0b111) << 10;
     putfslx(temp, 0, FSL_BLOCKING);
 
-    // 11111111111111111111111111111111
     // index of packet
+    // 11111111111111111111111111111111
     temp             = *(buffer + 5);
 
-    // 11111111111111111111111111111111
     // length of packet data
+    // 11111111111111111111111111111111
     temp             = *(buffer + 6);
     int len4         =  (temp >> 2);
 
-    // 1      11111111111111111111111111111
     // init   length/4 of packet data
+    // 1      11111111111111111111111111111
     temp             =  len4 | (to_initialise << 31);
     putfslx(temp, 0, FSL_BLOCKING);
 
     dataptr          =   buffer + 7;
-    
-    set_leds(flash);
-    flash = !flash;
+  
 
-    for (i = 0; i < len4; i ++) {
+    for (i = 0; i < len4; i ++, dataptr ++) {
 
-      temp = *(dataptr+i);
-
-      putfslx(temp, 0, FSL_BLOCKING);
+      putfslx(*dataptr, 0, FSL_BLOCKING);
 
     }
 
@@ -100,31 +127,34 @@ void send_test(void) {
 }
 
 
-int get_channel_ids() {
-  int accept = 0;
-  int channel_ids = 0;
-  while (!accept) {
-    char c; char c1 = '0'; char c2 = '5';
-    channel_ids = 0;
+void input_channel_ids() {
 
-    for (int i = 5; i <= 20; i++, c2++) {
-      if (c2 > '9') { c2 = '0'; c1 ++; }
+  char in_c, digit0, digit1; 
 
-      uart_send_string(UART, "Mix channel ");
-      uart_send_char(UART, c1); uart_send_char(UART, c2);
-      uart_send_string(UART, ": ");
+  PS("Press a key to start mixing up to 4 channels. Then use 1 or 0 to select yes or no\r\n")
+  uart_wait_char(UART);
 
-      int to_play = ((c = uart_wait_char(UART)) == '1' || c == 'y');
-      uart_send_string(UART, to_play ? "Yes\r\n" : "No\r\n");
-      channel_ids |= to_play << (20-i);
-    }
-    uart_send_string(UART, "Accept? ");
-    accept = ((c = uart_wait_char(UART)) == '1' || c == 'y');
-    uart_send_string(UART, "\r\n");
+  num_channels = 0; digit1 = '0'; digit0 = '5';
+  channels[0] = 0; channels[1] = 0; channels[2] = 0; channels[3] = 0;
+
+  // Loop to channel 20 or until num channels is 4
+  for (int i = 5; i <= 20 && num_channels < 4; i++, digit0++) {
+
+    // Increase character digits for output
+    if (digit0 > '9') { digit0 = '0'; digit1 ++; }
+    PS("Mix channel ") PC(digit1) PC(digit0) PS("/20: ")
+
+    // Wait until input character is 1 or 0
+    do { in_c = uart_wait_char(UART); } while (in_c != '1' && in_c != '0');
+
+    // Update channel array
+    if (in_c == '1') channels[num_channels ++] = i;
+
+    PS((in_c == '1') ? "Yes\r\n" : "No\r\n")
+
   }
 
-  uart_send_string(UART, "Job done\r\n");
-  return channel_ids;
+  PC(num_channels + '0') PS(" channels selected\r\n")
 }
 
 
@@ -137,21 +167,17 @@ int main(void) {
   *(MAC_TX1 + 1) = 0x00070000;
   mac_set_mac_address();
 
-  //int channel_ids = get_channel_ids();
-
 
   intc_enable_all_interrupts();
-  //buttons_enable_interrupts();
   switches_enable_interrupts();
-  //uart_enable_interrupts(UART);
   mac_enable_interrupts();
   send_test();
 
-  channel_id = ((int) get_switches()) + 1;
-  uart_print_hex_16(UART, channel_id);
-
-  volatile int counter = 0;
-  while (1) {}
+  set_channel_switches();
+  
+  while (1) {
+    input_channel_ids();
+  }
  
   return 0;
 }
